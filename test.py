b4d370acaf36fb03425f2d01b004ce169a54dc14
carlsonl2 <carlsonl2@nycstudents.net>
2025-09-08 09:24:15 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print("hello")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+
+
+def rectangle():
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+rectangle()
+
+def triangle(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+triangle(90)    
+    
